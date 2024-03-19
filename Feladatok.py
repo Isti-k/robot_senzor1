cb4cff655fc8fda117fbf0cc4788b8a435f411b6
Isti-k <kiscserkes03@gmail.com>
2024-03-19 11:10:40 +0100

    robot

---
+++ Feladatok.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor, InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile, Image
+
+
+class Feladatok():
+
+    def __init__(self):
+        # tégla
+        self.ev3 = EV3Brick()
+        # motorok
+        self.jm = Motor(Port.B)
+        self.bm = Motor(Port.C)
+        self.km = Motor(Port.A)
+        # szenzorok
+        self.cs = ColorSensor(Port.S3)
+        self.ts = TouchSensor(Port.S1)
+        self.gs = GyroSensor(Port.S2)
+        self.us = UltrasonicSensor(Port.S4)
+        #self.ir = InfraredSensor(Port.S4)
+
+        # dupla motorkezelő
+        self.robot = DriveBase(self.jm, self.bm, 55, 115)
+
+        #időzítő
+        self.ido=StopWatch()
+
+    def csipog(self):
+        self.ev3.speaker.beep()
+        
+    def vonalkovet(self):
+        #felvátva hajtom a motorokat egyik gyorsabb másik lassabb ha letér csere
+        while True:
+            if self.cs.reflection() > 35:
+                self.bm.run(200)
+                self.jm.run(100)
+            else:
+                self.jm.run(200)
+                self.bm.run(100)
+
+    def aku(self):
+        votl = self.ev3.battery.voltage()/1000
+        amper = self.ev3.battery.current()/1000
+
+        print("A feszültség értéke. "+str(votl)+"V.")
+        print("Az áramerőség: "+str(amper)+"A.")
+
+
+    def aku2(self):
+        votl = self.ev3.battery.voltage()/1000
+        amper = self.ev3.battery.current()/1000
+
+        self.ev3.screen.print("A feszültség értéke. \n"+str(votl)+"V.")
+        self.ev3.screen.print("Az áramerőség: \n"+str(amper)+"A.")
+        wait(1000)
+
+    def feladat1(self):
+        # asztallap széle: 59
+        # asztal széle felesem: 19
+        # asztal széle után: 0
+        # fekete vonal: 9
+        ut = (59 + 19)/2
+        while self.cs.reflection() > ut:
+            self.robot.drive(100,0)
+            print("Visszavert fény: ", self.cs.reflection(),".")
+        self.robot.stop(Stop.BRAKE)
+
+
+
+    def feladat2(self):
+        self.ido.reset()
+        ut = (59 + 19)/2
+        while self.cs.reflection() > ut:
+            self.robot.drive(100,0)
+            #print("Visszavert fény: ", self.cs.reflection(),".")
+        self.robot.stop(Stop.BRAKE)
+        elteltIdo = self.ido.time() # aktuális érték visszaadása
+        self.robot.drive(-100,0) # hátra
+        wait(elteltIdo)
+        self.robot.stop(Stop.BRAKE)
+
+
+    def feladat3():
+        while self.us.distance() > 100:
+            self.robot.drive(self.us.distance(), 0)
+        self.robot.stop(Stop.BRAKE)
